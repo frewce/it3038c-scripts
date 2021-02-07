678074c737dc29d9631b1ab66c8ba331416f5692
Cassidy Frew <frewce@mail.uc.edu>
2021-02-07 17:17:41 -0500

    new file:   powershell/sysinfo.ps1

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,14 @@
+﻿#$Hello = "Hello Powershell"
+function GetIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+    }
+
+
+$IP = GetIP
+$user = $env:USERDOMAIN
+$Date = Get-Date
+$this = $host.Version.Major
+
+$Body = "This machine's IP is $IP. User is $env:username. Host name is $user. Powershell version $this. Today's Date is $Date. "
+#Send-MailMessage -to "frewce@mail.uc.edu" -from "
+Write-Output $Body
