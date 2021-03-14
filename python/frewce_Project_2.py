044480353fa117dbe33e276a9a6f5a3d9a2389b5
frewce <77364820+frewce@users.noreply.github.com>
2021-03-13 19:06:08 -0500

    Update frewce_Project_2.py
    added changes to the wording and spelling

--- python/frewce_Project_2.py
+++ python/frewce_Project_2.py
@@ -3,14 +3,14 @@ import socket
 
 bomb = emoji.emojize(' :bomb:')
 thumb = emoji.emojize(' :red_heart:')
-computer = emoji.emojize(' :pager:') #if you have an outdated version of pip computer won't show so I used a pager instead
+computer = emoji.emojize(' :pager:') #if you have an outdated version of pip the computer emoji won't show so I used a pager instead
 
 
-user_input = (input("Are you done? "))
+user_input = (input("Would you like to look up a website's IP address? "))
 
 while (user_input == "no") or (user_input == "No"):
 
-    print ("ok, look up another.")
+    print ("Ok, look up another.")
     website = (input("Enter a" + str(bomb) + "website: (Ex: www.google.com)"))
     print(website + ': ' + socket.gethostbyname(website) + computer)
     user_input = (input("Are you done? "))
