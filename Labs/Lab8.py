99ba4abba1324d5e149f80613c7e6e505b393034
Cassidy Frew <frewce@mail.uc.edu>
2021-03-16 19:58:42 -0400

    Lab8 update

--- Labs/Lab8.py
+++ Labs/Lab8.py
@@ -2,16 +2,15 @@ import requests, re
 from bs4 import BeautifulSoup
 
 data = requests.get("https://global.bookwalker.jp/dea38393ef-adca-4375-a5cb-8a49e75d9219/that-time-i-got-reincarnated-as-a-slime-trinity-in-tempest-3/").content
-#print(data)
+
+#isolating and cleaning the title value
 soup = BeautifulSoup(data, 'html.parser')
 d = soup.find("h1", {"itemprop":"name"})
 title = d.contents[0]
 
-#title = d.string.strip()
+#isolating and cleaning the price value
 p = soup.find("p", {"class":"detail-price"})
 price = p.contents[0].strip()[4:]
 
-#price = soup.find("p", {"class":"detail-price"})
-#print(type(p))
 
 print("The item %s costs $%s." % (title, price))
