06867bb299a59db33e5fabfcc28d4d6deabd6010
frewce <77364820+frewce@users.noreply.github.com>
2021-04-20 23:36:30 -0400

    Update frewce_Project_3.py
    added comments

--- python/frewce_Project_3.py
+++ python/frewce_Project_3.py
@@ -4,16 +4,16 @@ import time
 from PIL import Image
 from PIL import ImageFilter
 
-cwd = os.path.expanduser('~\Desktop')
-print(cwd)
-
-print("This is a program to edit images from URLs.")
+#example of python user input, introducing to a user what the program does without reading the README
+print("This is a program to edit images from URLs.") 
 userinput = input("Paste a link of an image you want to edit. ")
 
+#using pillow to save the requested image and save in in the current directory
 im = urllib.request.urlretrieve(userinput, "sample.png")
 im = Image.open("sample.png")
 print(im.format, im.size, im.mode)
 
+#take the image and manipulate it for you to view, i used the time function to allow the image to be rendered as a temporary BMP file
 userinput1 = input("Do you want to Rotate the image, Flip the image(horizontally), Deepfry the image or Smooth the image? ")
 if (userinput1 == "rotate") or (userinput1 == "Rotate"):
     img = im.transpose(Image.ROTATE_90)
@@ -38,7 +38,7 @@ elif (userinput1 == "Deepfried") or (userinput1 == "deepfried"):
 else:
     print("Bye!")
 
-
+#i spent a fair amount of time trying to figure out how to save it to desktop on windows before i realized people used Linux machines in our class
 userinput2 = input("Are you doing ok today? ")
 if (userinput1 == "yes") or (userinput1 == "Yes"):
     print("Good! Because you don't get to keep the image.")
